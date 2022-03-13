f213d9a3ff43bbcc53e58f2e031afa66d17726fd
Sajjad <sajad.abedi@veriff.net>
2022-03-13 17:07:37 +0200

    Initial Commit

---
+++ VeriffLoadingAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VeriffLoadingAnimation
+//
+//  Created by Sajad Abedi on 13.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VeriffLoadingAnimation/VeriffLoadingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeriffLoadingAnimationApp.swift
+//  VeriffLoadingAnimation
+//
+//  Created by Sajad Abedi on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct VeriffLoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
